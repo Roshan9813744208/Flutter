5f26e80da5fa84b7b5b6d098e68ad7b66a60abf4
FlutLab.io <>
2021-06-26 11:14:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "My App",
       home: new Scaffold(
           appBar: new AppBar(
